2a75f7a5fbe0241197f8da904ab25e6d7eebf133
Alex Meub <alexmeub@gmail.com>
2022-03-08 22:43:28 -0800

    updated keyboard controller

--- controller.py
+++ controller.py
@@ -22,8 +22,8 @@ y_position = 100
 trigger_position = 0
 
 # Limits
-y_max = 160
-y_min = 100
+y_max = 180
+y_min = 50
 trigger_min = 0
 y_increment_unit = 2 # this determines speed of y axis movement
 
@@ -37,15 +37,16 @@ def activate_trigger( number ):
 
 def reset_defaults():
 	# Reset everything to known good values
-	kit.servo[y_axis].angle = y_min + 30
+	kit.servo[y_axis].angle = 50
 	kit.servo[trigger].angle = trigger_min
 	kit.continuous_servo[x_axis].throttle = 0
 
 # Handle Keyup events separately for x axis control
 # of continuous servos
 def on_key_release(key):
-	if key.name == "up" || key.name == "down":
-		print("up/down released")
+	print(key.name)
+	if "left" in key.name or "right" in key.name:
+		print("left/right released")
 		# Turn off
 		kit.continuous_servo[x_axis].throttle = 0
 
@@ -53,54 +54,62 @@ def on_key_release(key):
 keyboard.on_release(on_key_release, suppress=False)
 
 # Main Loop
-try:
-	reset_defaults()
-	running = True
 
-	while running:
+#reset_defaults()
+running = True
 
-		# Use while statements to prevent key repeat which is 
-		# the desired behavior for controlling servos 
-		# (we don't want servos to move after key is released)
-		while keyboard.is_pressed("up"):
-			print("UP")
-			if( y_position < y_max ):
-				y_position=y_position+y_increment_unit
-				kit.servo[y_axis].angle = y_position
-			else:	
-				print( "Y axis out of range" )
-				y_position = y_max
+print(kit.servo[y_axis].angle)
+# Set Y to current position
 
-		while keyboard.is_pressed("down"):
-			print("DOWN")
-			if( y_position > y_min ):
-				y_position=y_position-y_increment_unit
-				kit.servo[y_axis].angle = y_position
-			else:
-				print( "Y axis out of range" )
-				y_position = y_min	
+y_position = kit.servo[y_axis].angle	
+	
+while running:
+	
+	# Need to have this for some reason
+	time.sleep(0.02)
+	
+	# Use while statements to prevent key repeat which is 
+	# the desired behavior for controlling servos 
+	# (we don't want servos to move after key is released)
+	while keyboard.is_pressed("up"):
+		y_position=y_position+y_increment_unit
+		if( y_position > y_max ):
+			print("Y is over max")
+			y_position=y_position-y_increment_unit
+		else:	
+			kit.servo[y_axis].angle = y_position
+			time.sleep(0.02)
+			print( kit.servo[y_axis].angle )
 
-		while keyboard.is_pressed("left"):
-			print("LEFT")
-			kit.continuous_servo[x_axis].throttle = -1
+	while keyboard.is_pressed("down"):
+		y_position=y_position-y_increment_unit
+		if( y_position < y_min ):
+			print("Y is over max")
+			y_position=y_position+y_increment_unit
+		else:	
+			kit.servo[y_axis].angle = y_position
+			time.sleep(0.02)
+			print( kit.servo[y_axis].angle )
 
-		while keyboard.is_pressed("right"):
-			print("RIGHT")
-			kit.continuous_servo[x_axis].throttle = 1
+	while keyboard.is_pressed("left"):
+		#print("LEFT")
+		kit.continuous_servo[x_axis].throttle = -1
 
-	    while keyboard.is_pressed(" "):
-			print("SPACE")
-			time.sleep(1)
-			kit.servo[number].angle = 100
-			time.sleep(0.3)
-			# Set back to zero
-			kit.servo[number].angle = 0
+	while keyboard.is_pressed("right"):
+		#print("RIGHT")
+		kit.continuous_servo[x_axis].throttle = 1
 
-		while keyboard.is_pressed("q"):
-			print("Quitting...")
-			reset_defaults()
-			running = False
-			break
+	while keyboard.is_pressed(" "):
+		print("SPACE")
+		kit.servo[trigger].angle = 100
+		time.sleep(0.3)
+		# Set back to zero
+		kit.servo[trigger].angle = 0
 
-		#print( kit.continuous_servo[x_axis].throttle )
+	while keyboard.is_pressed("q"):
+		print("Quitting...")
+		kit.servo[y_axis].angle = 120
+		#reset_defaults()
+		running = False
+		break
 
